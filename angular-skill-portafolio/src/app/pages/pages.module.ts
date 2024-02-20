3d004963ae4b82263d9dc73c819eb338464b2939
Bairon Freire <bairon.f5@gmail.com>
2024-02-20 15:26:33 -0500

    Crear pagina de educación

--- angular-skill-portafolio/src/app/pages/pages.module.ts
+++ angular-skill-portafolio/src/app/pages/pages.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
 
 
 
 @NgModule({
   declarations: [
     AboutComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    EducationComponent
   ],
   imports: [
     CommonModule,
