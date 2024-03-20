66ddeaa221a56f29bac6d5946aebf73136bad5cd
Bairon Freire <bairon.f5@gmail.com>
2024-03-20 13:07:15 -0500

    Agregar encabezado a el layout principal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { CardTerminalComponent } from './components/card-terminal/card-terminal.component';
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent,
+    CardTerminalComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    SidebarComponent
+    SidebarComponent,
+    HeaderComponent,
+    CardTerminalComponent
   ]
 })
 export class SharedModule { }
